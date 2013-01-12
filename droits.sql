e4ed530346ee7cc5574886cf9ebebb7d4130d8da
Hakuri <lordman38@laposte.net>
2013-01-12 13:19:20 +0100

    maj droits

--- droits.sql
+++ droits.sql
@@ -2,14 +2,18 @@
 --DROITS DE DIRECTEUR TECHNIQUE-------------------------------------------------
 --------------------------------------------------------------------------------
 CREATE VIEW V_profdispo AS SELECT NumMono FROM MONITEURS WHERE NumMono NOT IN ENSEIGNE;
-
 GRANT ALL ON TYPECOURS TO skivite;
 GRANT ALL ON MONITEURS TO skivite;
 GRANT ALL ON COURS TO skivite;
+GRANT SELECT ON V_profdispo TO skivite;
+GRANT ALL ON ENSEIGNE TO skivite;
 --------------------------------------------------------------------------------
 --DROITS DE SECRETAIRE----------------------------------------------------------
 --------------------------------------------------------------------------------
-
+CREATE VIEW V_infocours AS SELECT * FROM TYPECOURS, COURS WHERE TYPECOURS.NumType=COURS.NumType;
+GRANT SELECT ON V_infocours TO tapevite;
+GRANT SELECT ON COURS TO tapevite;
+GRANT INSERT ON INSCRIT TO tapevite;
 --------------------------------------------------------------------------------
 --DROITS DE MONITEUR------------------------------------------------------------
 --------------------------------------------------------------------------------
