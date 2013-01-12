6694016a89429130029ab5e3446429bae1fd285a
RemyProjet <nagolya.dofus@gmail.com>
2013-01-12 16:02:48 +0100

    maj insert part1

--- insert.sql
+++ insert.sql
@@ -1,4 +1,6 @@
-test
+---------------------------------------------------------------------------------------------
+-- MONITEUR - (NumMono, Nom, Prenom, Adresse, Telephone )------------------------------------
+---------------------------------------------------------------------------------------------
 INSERT INTO MONITEUR * VALUES (1,'Ange','Alceste','Arles','0476598465');
 INSERT INTO MONITEUR * VALUES (2,'Beri','Bernard','Bordaux','0476458798');
 INSERT INTO MONITEUR * VALUES (3,'Cipa','Charline','Chateau-Bois','0476542151');
@@ -9,76 +11,124 @@ INSERT INTO MONITEUR * VALUES (7,'Gips','Gaetan','Grenoble','0476562312');
 INSERT INTO MONITEUR * VALUES (8,'Haul','Helene','Havre','0476946184');
 INSERT INTO MONITEUR * VALUES (9,'Iron','Isaac','Issoire','0478652375');
 INSERT INTO MONITEUR * VALUES (10,'Loso','Léa','Lille','0476872191');
+INSERT INTO MONITEUR * VALUES (11,'Martinez','Matthieu','Marseille','0476597859');
+INSERT INTO MONITEUR * VALUES (12,'Nojent','Norbert','Neaux','0476452103');
+INSERT INTO MONITEUR * VALUES (13,'Oriez','Odile','Orange','0476540604');
+INSERT INTO MONITEUR * VALUES (14,'Perrez','Pierre','Parlan','0487592559');
+INSERT INTO MONITEUR * VALUES (15,'Quedreux','Quentin','Quezac','0476959941');
+INSERT INTO MONITEUR * VALUES (16,'Ribot','Raoul','Rieux','0476982536');
+INSERT INTO MONITEUR * VALUES (17,'Sapin','Sophie','Saint Maurice','0476567410');
+INSERT INTO MONITEUR * VALUES (18,'Thevenon','Thierry','Toulouse','0476941230');
+INSERT INTO MONITEUR * VALUES (19,'Umbert','Ursule','Ussac','0478653692');
+INSERT INTO MONITEUR * VALUES (20,'Vernay','Voilette','voiron','0476872514');
 
+---------------------------------------------------------------------------------------------
+-- ELEVE - (	NumEleve, Nom, Prenom, AdresseStation, Mobile )------------------------------
+---------------------------------------------------------------------------------------------
+INSERT INTO ELEVE * VALUES (1,'Achard','Lucille','Aime La Plagne','0645862100');
+INSERT INTO ELEVE * VALUES (2,'Adonel','Paul','Briançon','0645862101');
+INSERT INTO ELEVE * VALUES (3,'Agier','Kevin','Flaine','0645862102');
+INSERT INTO ELEVE * VALUES (4,'Bail','Lucas','Aime La Plagne','0645862103');
+INSERT INTO ELEVE * VALUES (5,'Bailly','Frederic','Briançon','0645862104');
+INSERT INTO ELEVE * VALUES (6,'Baldit','Jeremie','Briançon','0645862105');
+INSERT INTO ELEVE * VALUES (7,'Baron','Coralie','Flaine','0645862106');
+INSERT INTO ELEVE * VALUES (8,'Baron','Deborah','Meribel','0645862107');
+INSERT INTO ELEVE * VALUES (9,'Buzenet','Julien','Briançon','0645862108');
+INSERT INTO ELEVE * VALUES (10,'Cagnon','Julien','Flaine','0645862109');
+INSERT INTO ELEVE * VALUES (11,'Calin','Laurie','Briançon','0645862110');
+INSERT INTO ELEVE * VALUES (12,'Canu','Romain','Aime La Plagne','0645862111');
+INSERT INTO ELEVE * VALUES (13,'Celle','Amandine','Aime La Plagne','0645862112');
+INSERT INTO ELEVE * VALUES (14,'Celle','David','Aime La Plagne','0645862113');
+INSERT INTO ELEVE * VALUES (15,'Dalfin','Stephanie','Briançon','0645862114');
+INSERT INTO ELEVE * VALUES (16,'Damien','Antoine','Flaine','0645862115');
+INSERT INTO ELEVE * VALUES (17,'Damon','Marine','Briançon','0645862116');
+INSERT INTO ELEVE * VALUES (18,'Debard','Alexis','Briançon','0645862117');
+INSERT INTO ELEVE * VALUES (19,'Delange','Kevin','Meribel','0645862118');
+INSERT INTO ELEVE * VALUES (20,'Delcellier','Morgan','Flaine','0645862119');
+INSERT INTO ELEVE * VALUES (21,'Depret','Alice','Meribel','0645862120');
+INSERT INTO ELEVE * VALUES (22,'Echard','Victor','Aime La Plagne','0645862121');
+INSERT INTO ELEVE * VALUES (23,'Empereur','Kevin','Briançon','0645862122');
+INSERT INTO ELEVE * VALUES (24,'Eparvier','Benjamin','Flaine','0645862123');
+INSERT INTO ELEVE * VALUES (25,'Escoffier','Claire','Aime La Plagne','0645862124');
+INSERT INTO ELEVE * VALUES (26,'Eynard','Ines','Meribel','0645862125');
+INSERT INTO ELEVE * VALUES (27,'Eyraud','Marie','Meribel','0645862126');
+INSERT INTO ELEVE * VALUES (28,'Ezzahid','Fatima','Flaine','0645862127');
+INSERT INTO ELEVE * VALUES (29,'Fabier','Camille','Risoul','0645862128');
+INSERT INTO ELEVE * VALUES (30,'Fabre','Leslie','Risoul','0645862129');
+INSERT INTO ELEVE * VALUES (31,'Falcon','Estelle','Briançon','0645862130');
+INSERT INTO ELEVE * VALUES (32,'Falcon','Fanny','Aime La Plagne','0645862131');
+INSERT INTO ELEVE * VALUES (33,'Fanget','Celine','Flaine','0645862132');
+INSERT INTO ELEVE * VALUES (34,'Farcy','Elena','Flaine','0645862133');
+INSERT INTO ELEVE * VALUES (35,'Faure','Julien','Flaine','0645862134');
+INSERT INTO ELEVE * VALUES (36,'Faure','Maud','Briançon','0645862135');
+INSERT INTO ELEVE * VALUES (37,'Favier','Charline','Aime La Plagne','0645862136');
+INSERT INTO ELEVE * VALUES (38,'Ferry','Anthony','Aime La Plagne','0645862137');
+INSERT INTO ELEVE * VALUES (39,'Gabet','Elsa','Briançon','0645862138');
+INSERT INTO ELEVE * VALUES (40,'Gaillard','Constance','Briançon','0645862139');
+INSERT INTO ELEVE * VALUES (41,'Gaillard','Axel','Aime La Plagne','0645862140');
+INSERT INTO ELEVE * VALUES (42,'Gaillard','Melanie','Flaine','0645862141');
+INSERT INTO ELEVE * VALUES (43,'Genin','Sandra','Meribel','0645862142');
+INSERT INTO ELEVE * VALUES (44,'Germain','Caroline','Risoul','0645862143');
+INSERT INTO ELEVE * VALUES (45,'Giraud','Xavier','Briançon','0645862144');
+INSERT INTO ELEVE * VALUES (46,'Giron','Eva','Aime La Plagne','0645862145');
+INSERT INTO ELEVE * VALUES (47,'Goujon','Elisa','Aime La Plagne','0645862146');
+INSERT INTO ELEVE * VALUES (48,'Guyon','Laura','Flaine','0645862147');
+INSERT INTO ELEVE * VALUES (49,'Hamour','Roslene','Briançon','0645862148');
+INSERT INTO ELEVE * VALUES (50,'Harizi','Sarah','Meribel','0645862149');
+INSERT INTO ELEVE * VALUES (51,'Iaboni','Gabriele','Flaine','0645862150');
+INSERT INTO ELEVE * VALUES (52,'Ibars','Diego','Aime La Plagne','0645862151');
+INSERT INTO ELEVE * VALUES (53,'Imbert','Elodie','Aime La Plagne','0645862152');
+INSERT INTO ELEVE * VALUES (54,'Inal','Levent','Briançon','0645862153');
+INSERT INTO ELEVE * VALUES (55,'Ingrao','William','Flaine','0645862154');
+INSERT INTO ELEVE * VALUES (56,'Inigo','Irene','Risoul','0645862155');
+INSERT INTO ELEVE * VALUES (57,'Iotefa','Candice','Meribel','0645862156');
+INSERT INTO ELEVE * VALUES (58,'Isnard','Christophe','Briançon','0645862157');
+INSERT INTO ELEVE * VALUES (59,'Izem','Kevin','Flaine','0645862158');
+INSERT INTO ELEVE * VALUES (60,'Jabrin','Camille','Briançon','0645862159');
+INSERT INTO ELEVE * VALUES (61,'Jac','Anais','Briançon','0645862160');
+INSERT INTO ELEVE * VALUES (62,'Jacquet','Marion','Briançon','0645862161');
+INSERT INTO ELEVE * VALUES (63,'Jesus','Gabin','Risoul','0645862162');
+INSERT INTO ELEVE * VALUES (64,'Joly','Corentin','Risoul','0645862163');
+INSERT INTO ELEVE * VALUES (65,'Joly','Justine','Flaine','0645862164');
+INSERT INTO ELEVE * VALUES (66,'Juniot','Benjamin','Briançon','0645862165');
+INSERT INTO ELEVE * VALUES (67,'Kante','Awa','Flaine','0645862166');
+INSERT INTO ELEVE * VALUES (68,'Kirsch','Valentin','Meribel','0645862167');
+INSERT INTO ELEVE * VALUES (69,'Kuzu','Selkan','Aime La Plagne','0645862168');
+INSERT INTO ELEVE * VALUES (70,'Laboure','Mickael','Briançon','0645862169');
+INSERT INTO ELEVE * VALUES (71,'Labrosse','Stephen','Flaine','0645862170');
+INSERT INTO ELEVE * VALUES (72,'Lacroix Aurelien','Aime La Plagne','0645862171');
+INSERT INTO ELEVE * VALUES (73,'Laporte','Johnny','Flaine','0645862172');
+INSERT INTO ELEVE * VALUES (74,'Laporte','Manon','Flaine','0645862173');
+INSERT INTO ELEVE * VALUES (75,'Ly','Tchang','Aime La Plagne','0645862174');
+INSERT INTO ELEVE * VALUES (76,'Lyonnet','Mathilde','Aime La Plagne','0645862175');
+INSERT INTO ELEVE * VALUES (77,'Machin','Florian','Risoul','0645862176');
+INSERT INTO ELEVE * VALUES (78,'Macit','Nazan','Briançon','0645862177');
+INSERT INTO ELEVE * VALUES (79,'Magat','Boris','Flaine','0645862178');
+INSERT INTO ELEVE * VALUES (80,'Nabaile','Laetitia','Meribel','0645862179');
+INSERT INTO ELEVE * VALUES (81,'Naro','Clemence','Risoul','0645862180');
+INSERT INTO ELEVE * VALUES (82,'Navarro','Lolita','Aime La Plagne','0645862181');
+INSERT INTO ELEVE * VALUES (83,'Ohana','Veronique','Aime La Plagne','0645862182');
+INSERT INTO ELEVE * VALUES (84,'Ohier','Cindy','Aime La Plagne','0645862183');
+INSERT INTO ELEVE * VALUES (85,'Ovigue','Cedric','Briançon','0645862184');
+INSERT INTO ELEVE * VALUES (86,'Paci','Jessica','Flaine','0645862185');
+INSERT INTO ELEVE * VALUES (87,'Paillet','Christopher','Aime La Plagne','0645862186');
+INSERT INTO ELEVE * VALUES (88,'Pavese','Clara','Aime La Plagne','0645862187');
+INSERT INTO ELEVE * VALUES (89,'Peguin','Cyril','Aime La Plagne','0645862188');
+INSERT INTO ELEVE * VALUES (90,'Quoy','Jordan','Aime La Plagne','0645862189');
+INSERT INTO ELEVE * VALUES (91,'Raffin','Alexia','Aime La Plagne','0645862190');
+INSERT INTO ELEVE * VALUES (92,'Renard','Emma','Flaine','0645862191');
+INSERT INTO ELEVE * VALUES (93,'Renaud','Chloe','Flaine','0645862192');
+INSERT INTO ELEVE * VALUES (94,'Sabot','Colin','Briançon','0645862193');
+INSERT INTO ELEVE * VALUES (95,'Talji','Cherine','Briançon','0645862194');
+INSERT INTO ELEVE * VALUES (96,'Valette','Marine','Risoul','0645862195');
+INSERT INTO ELEVE * VALUES (97,'Webber','Sorayya','Flaine','0645862196');
+INSERT INTO ELEVE * VALUES (98,'Xia','Rebecca','Briançon','0645862197');
+INSERT INTO ELEVE * VALUES (99,'Younsi','Linda','Aime La Plagne','0645862198');
+INSERT INTO ELEVE * VALUES (100,'Zec','Victoria','Aime La Plagne','0645862199');
 
-
-
-
-
-
-
-
-
-INSERT INTO ELEVE * VALUES (1,'','','','');
-INSERT INTO ELEVE * VALUES (2,'','','','');
-INSERT INTO ELEVE * VALUES (3,'','','','');
-INSERT INTO ELEVE * VALUES (4,'','','','');
-INSERT INTO ELEVE * VALUES (5,'','','','');
-INSERT INTO ELEVE * VALUES (6,'','','','');
-INSERT INTO ELEVE * VALUES (7,'','','','');
-INSERT INTO ELEVE * VALUES (8,'','','','');
-INSERT INTO ELEVE * VALUES (9,'','','','');
-INSERT INTO ELEVE * VALUES (10,'','','','');
-INSERT INTO ELEVE * VALUES (11,'','','','');
-INSERT INTO ELEVE * VALUES (12,'','','','');
-INSERT INTO ELEVE * VALUES (13,'','','','');
-INSERT INTO ELEVE * VALUES (14,'','','','');
-INSERT INTO ELEVE * VALUES (15,'','','','');
-INSERT INTO ELEVE * VALUES (16,'','','','');
-INSERT INTO ELEVE * VALUES (17,'','','','');
-INSERT INTO ELEVE * VALUES (18,'','','','');
-INSERT INTO ELEVE * VALUES (19,'','','','');
-INSERT INTO ELEVE * VALUES (20,'','','','');
-INSERT INTO ELEVE * VALUES (21,'','','','');
-INSERT INTO ELEVE * VALUES (22,'','','','');
-INSERT INTO ELEVE * VALUES (23,'','','','');
-INSERT INTO ELEVE * VALUES (24,'','','','');
-INSERT INTO ELEVE * VALUES (25,'','','','');
-INSERT INTO ELEVE * VALUES (26,'','','','');
-INSERT INTO ELEVE * VALUES (27,'','','','');
-INSERT INTO ELEVE * VALUES (28,'','','','');
-INSERT INTO ELEVE * VALUES (29,'','','','');
-INSERT INTO ELEVE * VALUES (30,'','','','');
-INSERT INTO ELEVE * VALUES (31,'','','','');
-INSERT INTO ELEVE * VALUES (32,'','','','');
-INSERT INTO ELEVE * VALUES (33,'','','','');
-INSERT INTO ELEVE * VALUES (34,'','','','');
-INSERT INTO ELEVE * VALUES (35,'','','','');
-INSERT INTO ELEVE * VALUES (36,'','','','');
-INSERT INTO ELEVE * VALUES (37,'','','','');
-INSERT INTO ELEVE * VALUES (38,'','','','');
-INSERT INTO ELEVE * VALUES (39,'','','','');
-INSERT INTO ELEVE * VALUES (40,'','','','');
-INSERT INTO ELEVE * VALUES (41,'','','','');
-INSERT INTO ELEVE * VALUES (42,'','','','');
-INSERT INTO ELEVE * VALUES (43,'','','','');
-INSERT INTO ELEVE * VALUES (44,'','','','');
-INSERT INTO ELEVE * VALUES (45,'','','','');
-INSERT INTO ELEVE * VALUES (46,'','','','');
-INSERT INTO ELEVE * VALUES (47,'','','','');
-INSERT INTO ELEVE * VALUES (48,'','','','');
-INSERT INTO ELEVE * VALUES (49,'','','','');
-INSERT INTO ELEVE * VALUES (50,'','','','');
-
-
-
-
-
-
-
-
-
-
+---------------------------------------------------------------------------------------------
+-- TYPECOURS - (NumType, Discipline, Public, Niveau )----------------------------------------
+---------------------------------------------------------------------------------------------
 INSERT INTO TYPECOURS * VALUES (1,'Alpin','Enfant','piou-piou');
 INSERT INTO TYPECOURS * VALUES (2,'Alpin','Enfant','ourson');
 INSERT INTO TYPECOURS * VALUES (3,'Alpin','Enfant','flocon');
@@ -100,45 +150,43 @@ INSERT INTO TYPECOURS * VALUES (18,'Snowboard','+8ans','snowboard2');
 INSERT INTO TYPECOURS * VALUES (19,'Snowboard','+8ans','snowboard3');
 INSERT INTO TYPECOURS * VALUES (20,'Snowboard','+8ans','compétition');
 
+---------------------------------------------------------------------------------------------
+-- COURS - (	NumCours, DateDebut, DateFin, NumType, LieuRDV, HeureDeb, HeurFin )----------
+---------------------------------------------------------------------------------------------
+INSERT INTO COURS * VALUES (1,'07/01/2013','07/01/2013','1','','','');
+INSERT INTO COURS * VALUES (2,'07/01/2013','12/01/2013','1','','','');
+INSERT INTO COURS * VALUES (3,'07/01/2013','13/01/2013','2','','','');
+INSERT INTO COURS * VALUES (4,'07/01/2013','08/01/2013','3','','','');
+INSERT INTO COURS * VALUES (5,'07/01/2013','12/01/2013','5','','','');
+INSERT INTO COURS * VALUES (6,'07/01/2013','12/01/2013','4','','','');
+INSERT INTO COURS * VALUES (7,'07/01/2013','10/01/2013','8','','','');
+INSERT INTO COURS * VALUES (8,'07/01/2013','12/01/2013','9','','','');
+INSERT INTO COURS * VALUES (9,'07/01/2013','111/01/2013','6','','','');
+INSERT INTO COURS * VALUES (10,'07/01/2013','11/01/2013','9','','','');
+INSERT INTO COURS * VALUES (11,'09/01/2013','12/01/2013','10','','','');
+INSERT INTO COURS * VALUES (12,'09/01/2013','09/01/2013','15','','','');
+INSERT INTO COURS * VALUES (13,'09/01/2013','10/01/2013','13','','','');
+INSERT INTO COURS * VALUES (14,'09/01/2013','11/01/2013','12','','','');
+INSERT INTO COURS * VALUES (15,'09/01/2013','12/01/2013','11','','','');
+INSERT INTO COURS * VALUES (16,'10/01/2013','13/01/2013','14','','','');
+INSERT INTO COURS * VALUES (17,'10/01/2013','12/01/2013','20','','','');
+INSERT INTO COURS * VALUES (18,'10/01/2013','12/01/2013','17','','','');
+INSERT INTO COURS * VALUES (19,'10/01/2013','11/01/2013','16','','','');
+INSERT INTO COURS * VALUES (20,'10/01/2013','12/01/2013','18','','','');
+INSERT INTO COURS * VALUES (21,'10/01/2013','12/01/2013','19','','','');
+INSERT INTO COURS * VALUES (22,'10/01/2013','12/01/2013','8','','','');
+INSERT INTO COURS * VALUES (23,'10/01/2013','12/01/2013','5','','','');
+INSERT INTO COURS * VALUES (24,'10/01/2013','12/01/2013','20','','','');
+INSERT INTO COURS * VALUES (25,'11/01/2013','12/01/2013','20','','','');
+INSERT INTO COURS * VALUES (26,'11/01/2013','12/01/2013','9','','','');
+INSERT INTO COURS * VALUES (27,'11/01/2013','13/01/2013','8','','','');
+INSERT INTO COURS * VALUES (28,'11/01/2013','13/01/2013','1','','','');
+INSERT INTO COURS * VALUES (29,'11/01/2013','13/01/2013','2','','','');
+INSERT INTO COURS * VALUES (30,'12/01/2013','13/01/2013','6','','','');
 
-
-
-
-
-
-
-
-
-INSERT INTO COURS * VALUES (1,'','','','');
-INSERT INTO COURS * VALUES (2,'','','','');
-INSERT INTO COURS * VALUES (3,'','','','');
-INSERT INTO COURS * VALUES (4,'','','','');
-INSERT INTO COURS * VALUES (5,'','','','');
-INSERT INTO COURS * VALUES (6,'','','','');
-INSERT INTO COURS * VALUES (7,'','','','');
-INSERT INTO COURS * VALUES (8,'','','','');
-INSERT INTO COURS * VALUES (9,'','','','');
-INSERT INTO COURS * VALUES (10,'','','','');
-INSERT INTO COURS * VALUES (11,'','','','');
-INSERT INTO COURS * VALUES (12,'','','','');
-INSERT INTO COURS * VALUES (13,'','','','');
-INSERT INTO COURS * VALUES (14,'','','','');
-INSERT INTO COURS * VALUES (15,'','','','');
-INSERT INTO COURS * VALUES (16,'','','','');
-INSERT INTO COURS * VALUES (17,'','','','');
-INSERT INTO COURS * VALUES (18,'','','','');
-INSERT INTO COURS * VALUES (19,'','','','');
-INSERT INTO COURS * VALUES (20,'','','','');
-
-
-
-
-
-
-
-
-
-
+---------------------------------------------------------------------------------------------
+-- ENSEIGNE - (NumCours, NumMono) -----------------------------------------------------------
+---------------------------------------------------------------------------------------------
 INSERT INTO ENSEIGNE * VALUES (,);
 INSERT INTO ENSEIGNE * VALUES (,);
 INSERT INTO ENSEIGNE * VALUES (,);
@@ -158,4 +206,4 @@ INSERT INTO ENSEIGNE * VALUES (,);
 INSERT INTO ENSEIGNE * VALUES (,);
 INSERT INTO ENSEIGNE * VALUES (,);
 INSERT INTO ENSEIGNE * VALUES (,);
-1
+
